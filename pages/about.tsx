9b5b4f3b4fe27d817bc7426a007c19c64baf318c
Shuta Kumano <shoota.dev@gmail.com>
2020-06-18 00:37:59 +0900

    fix error

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const AboutPage: React.FC = () => {
   return <div>I am shoota</div>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
+
 const HomePage: React.FC = () => {
   return <div>hello next.js</div>
 }
-
 
 export default HomePage
